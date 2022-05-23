88d347e68219831a48d31d23b6be7539132bbc8e
Aüh <93604094+Liam-McRae@users.noreply.github.com>
2022-05-23 17:23:26 +0000

    finished

--- script.js
+++ script.js
@@ -1,72 +1,113 @@
-
-
 function guessingGame(){
   
   const guessingDom = document.getElementById("guessing");
   const playingDom  = document.getElementById("playing");
+  const finalDom    = document.getElementById("final");
   const guessDom    = document.getElementById("guess");
   const triesDom    = document.getElementById("tries");
   const hintDom     = document.getElementById("hint");
   const score       = document.getElementById("score");
   const ansDom      = document.getElementById("answer");
   const yesDom      = document.getElementById("yes");
+  const noDom       = document.getElementById("no");
+  const gamesDom    = document.getElementById("games");
   const guessesDom  = document.getElementById("guesses");
+  const averageDom  = document.getElementById("avg");
   const bestDom     = document.getElementById("best");
+  let guessing;
+  let answer = -1;
+  let tries;
+  let totalGames = 0;
+  let totalGuesses = 0;
+  let bestGame = 9999;
 
-  let answer = -1; 
-  let tries  = 0;
-  play();
+  playingDom.style.display = "none";
+  finalDom.style.display = "none";
+  start();
 
-  guessDom.addEventListener("change",makeGuess);
-  yesDom.addEventListener("click",play);
+  guessDom.addEventListener("change", tryGuess);
+  yesDom.addEventListener("click", nextRound);
+  noDom.addEventListener("click", finish);
   
-  function play(){
-    guessDom.setAttribute("placeholder", "1-100");
-    hintDom.innerHTML = "Give it a guess!";
-    guessDom.value = "";
-    guessingDom.style.display = "unset";
-    playingDom.style.display = "none";
 
-    answer = Math.floor(Math.random() * 100) +1;
-    ansDom.value = answer;
-    tries = 1;
+
+
+  function start() {
+
+    guessing = true;
+    answer = Math.floor((Math.random() * 100)) + 1;
+    tries = 0;
   }
-  function setTries(soFar){
-    return soFar;
-  }
-  function makeGuess(e){
-    this.setAttribute("placeholder",this.value);
+
+  function tryGuess(){
+    guess = guessDom.value;
+    if(guessing) {
+      if(guess > 0 && guess <= 100) {
+        tries++;
+        triesDom.value = tries;
     
-    if      (this.value < answer) hintDom.innerHTML = "HIGHER! The answer is higher!";
-    else if (this.value > answer) hintDom.innerHTML = "LOWER! The answer is lower!";
-    else    showGameResults(tries);
-  
-    triesDom.value = tries++;
-    this.value = "";
-
-    setTimeout(function(){
-      guessDom.focus();
-    },10);
+        if(guess > answer) {
+          hintDom.innerHTML = "Too High!";
+          //console.log(guess + " > " + answer);
+        } else if (guess < answer) {
+          hintDom.innerHTML = "Too low!";
+          //console.log(guess + " < " + answer);
+        } else {
+          totalGames++;
+          totalGuesses += tries;
+          if(tries < bestGame) {
+            bestGame = tries;
+          }          
+          guessing = false;
+          hintDom.innerHTML = "Correct!";  
+          correct = true;
+          ansDom.value = answer;
+          score.value = tries;
+          guessingDom.style.display = "none";
+          playingDom.style.display = "initial";
+          guessDom.value = null;
+          triesDom.value = 0;
+          
+        } 
+      } else {
+        hintDom.innerHTML = "That value is out of bounds.\nPlease choose a number between 1 and 100.";
+      }
+    }
   }
 
-  function showGameResults(tries){
-    guessingDom.style.display = "none";
-    playingDom.style.display  = "unset";
-    score.value = tries;
-    guessesDom.value = tries + parseInt(guessesDom.value);
-    bestDom.value = getBest(tries);
-    gamesDom.value = parseInt(gamesDom.value) + 1;
-    yesDom.focus();
+
+
+  function nextRound() {  
+    if(!guessing) {
+      playingDom.style.display = "none";
+      start();
+      hintDom.innerHTML = "Give it a guess!";
+      guessingDom.style.display = "initial";
+      guessing = true;
+      ansDom.value = null;
+      score.value = null;
+    }
   }
-  
-  function getBest(count){
-    const check = parseInt(bestDom.value);
-    if(check === 0 || count < check) return count;
-    else return check;
+
+
+  function finish() {
+    if(!guessing) {
+      playingDom.style.display = "none";
+      gamesDom.value = totalGames;
+      guessesDom.value = totalGuesses;
+      averageDom.value = totalGuesses / totalGames;
+      bestDom.value = bestGame;
+      finalDom.style.display = "initial";
+    }
   }
-  
+
+
+
+
+
+
+
 }
-
 
 
 guessingGame();
